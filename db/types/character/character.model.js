9490f0380d809cafa55f9c5f1a93071fd967909b
alex <alexliu12@gmail.com>
2019-04-05 20:39:49 -0700

    [prog] added schemas and models in separate folder
    -character, smashers added to 'types' folder
    -deleted old schema.js and models.js

---
+++ db/types/character/character.model.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../index');
+
+const Character = sequelize.define('character', {
+  id: { type: Sequelize.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+  name: { type: Sequelize.STRING, allowNull: false },
+  weight: { type: Sequelize.INTEGER, allowNull: false },
+  pic_url: { type: Sequelize.STRING, allowNull: false },
+  big_url: { type: Sequelize.STRING, allowNull: false },
+  zero_rank: { type: Sequelize.STRING, allowNull: false },
+  esam_rank: { type: Sequelize.STRING, allowNull: false },
+  leffen_rank: { type: Sequelize.STRING, allowNull: false },
+},
+{
+  timestamps: false,
+});
+
+module.exports = { Character };
---
+++ db/types/prosmasher/prosmasher.model.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../index');
+
+const ProSmasher = sequelize.define('prosmasher', {
+  id: { type: Sequelize.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+  name: { type: Sequelize.STRING, allowNull: false },
+  pic_url: { type: Sequelize.STRING, allowNull: false },
+  description: { type: Sequelize.TEXT, allowNull: false },
+  main: { type: Sequelize.ARRAY, allowNull: false },
+},
+{
+  timestamps: false,
+});
+
+module.exports = { ProSmasher };
