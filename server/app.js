9674ca2ed3537a2fb000e0f0d355d3d31c1a9a45
alex <alexliu12@gmail.com>
2019-05-17 01:39:11 -0700

    [prog] completes successful server startup
    - squashes server start bug
    - using extend keyword on query and mutation for every gql file [fix: first one does not have extend]

--- server/app.js
+++ server/app.js
@@ -1,10 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// const parser = require('body-parser');
-// const cors = require('cors');
-// const router = require('./router');
-// const graphqlHTTP = require('express-graphql');
-// const schema = require('../db/schema');
 const { ApolloServer } = require('apollo-server');
 const { schemaLoader } = require('./util.js');
 const character = require('../db/types/character/character.resolvers')
@@ -12,14 +5,7 @@ const hrsmasher = require('../db/types/hrsmasher/hrsmasher.resolvers')
 const prosmasher = require('../db/types/prosmasher/prosmasher.resolvers')
 const { merge } = require('lodash');
 
-// const app = express();
-
-// app.use(cors());
-// app.use(parser.json());
-// app.use(parser.urlencoded({ extended: true }));
-
 module.exports = {
-
   start : async () => {
     const rootSchema = `
     schema {
@@ -35,23 +21,14 @@ module.exports = {
       .catch(err => {
         console.log('ERROR', err)
       })
-    
+
     const server = new ApolloServer({
       typeDefs: [rootSchema, ...schemaTypes],
       resolvers: merge({}, character, hrsmasher, prosmasher),
     })
     
     server.listen({ port: 1025 }).then(({ url }) => {
-      console.log(`🚀 Server ready at ${url}`)
+      console.log(`🚀  Server ready at ${url}`)
     });
   }
 } 
-// app.use('/api', router);
-// app.use('/graphql', graphqlHTTP({
-//   schema,
-//   graphiql: true
-// }));
-
-// app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// module.exports = app;
